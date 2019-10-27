1f3ed0875e92638c1d0d78e5ca4e22241b982b9d
Mario Ochoa <mario8ato@hotmail.com>
2019-10-27 02:51:13 -0600

    web google maps

---
+++ Web/jquery.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>load demo</title>
+  <style>
+  body {
+    font-size: 12px;
+    font-family: Arial;
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+	<script>
+	    $(function() { // shorthand for `$(document).ready(function() {
+	        $("#rightPan").load("place_searches.html");
+	    });
+	</script>
+
+</body>
+</html>
---
+++ Web/js/recorrido.js
@@ -0,0 +1,57 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(
+        document.getElementById('map'), {
+            center: new google.maps.LatLng(19.320793, -99.143667),
+            zoom: 11.5
+        });
+
+    var iconBase =
+        'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    var features = [
+
+        {
+            position: new google.maps.LatLng(19.4403582, -99.1537393),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(19.358721, -99.188757),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(19.3297952, -99.1629129),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(19.2492593, -99.1273761),
+            type: 'parking'
+        },
+
+    ];
+
+    // Create markers.
+
+    for (var i = 0; i < features.length; i++) {
+        console.log(i);
+        console.log(features[i].position);
+        console.log("Map: " + map);
+
+        display_marker(features[i].position, i, map, labels[i % labels.length]);
+
+    }
+}
+
+display_marker = function(pos, i, mapita, label) {
+    setTimeout(
+        function() {
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                label: label,
+                map: mapita,
+            });
+
+        }, i * 1000);
+
+}
