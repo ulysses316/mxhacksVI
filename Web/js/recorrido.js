4ae281db6034c9ab03f3ec4b6e87f376ed93c711
Mario Ochoa <mario8ato@hotmail.com>
2019-10-27 03:11:25 -0600

    punteros

--- Web/js/recorrido.js
+++ Web/js/recorrido.js
@@ -12,18 +12,36 @@ function initMap() {
 
     var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+    var delegacion = [{
+            name: "CUH"
+        },
+        {
+            name: "AOB"
+        },
+        {
+            name: "COY"
+        },
+        {
+            name: "TLP"
+        }
+    ];
+
     var features = [
 
         {
+            // cuah
             position: new google.maps.LatLng(19.4403582, -99.1537393),
             type: 'parking'
         }, {
+            // Alvaro obregon
             position: new google.maps.LatLng(19.358721, -99.188757),
             type: 'parking'
         }, {
+            // Coyoacan
             position: new google.maps.LatLng(19.3297952, -99.1629129),
             type: 'parking'
         }, {
+            // tlalpan
             position: new google.maps.LatLng(19.2492593, -99.1273761),
             type: 'parking'
         },
@@ -37,18 +55,19 @@ function initMap() {
         console.log(features[i].position);
         console.log("Map: " + map);
 
-        display_marker(features[i].position, i, map, labels[i % labels.length]);
+        // display_marker(features[i].position, i, map, labels[i % labels.length]);
+        display_marker(features[i].position, i, map, delegacion[i % labels.length].name);
 
     }
 }
 
-display_marker = function(pos, i, mapita, label) {
+display_marker = function(pos, i, mapita, name) {
     setTimeout(
         function() {
 
             var marker = new google.maps.Marker({
                 position: pos,
-                label: label,
+                label: name,
                 map: mapita,
             });
 
